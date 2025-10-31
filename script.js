420b0384a74f56a586c64fdd34f03a2e63adac34
hemrajjhariya1 <hemrajjhariya1@gmail.com>
2025-10-31 09:08:19 +0530

    intial value added in js code

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const gameArena = document.getElementById("game-arena")
+    const arenaSize = 600; 
+    const cellSize = 20;
+    let score = 0; // score of the game & let var changeble
+    let gameStarted = false; // game status
+    let food = {x: 300, y: 200}; // {x:15*20, y:10*20} -> cell coordinate -> pixels (1 cell ka size 20px ka isiliye jis position par food rakhenge usme 20 ka multiply karenge tan pixel me convert hoga aur browser pixcel hi samajhta hai)
+    let snake = [{x:160, y:200}, {x:140, y:200}, {x:120, y:200}]
+
+});
